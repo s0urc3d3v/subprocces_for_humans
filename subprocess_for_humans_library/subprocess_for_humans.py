eed5060efee49910fbf494f854e833164b52575e
s0urc3d3v3l0pm3nt <author@techredesigned.com>
2017-01-08 15:11:01 -0500

    hinzufuegen eine Warnung fuer ein Method

--- subprocess_for_humans_library/subprocess_for_humans.py
+++ subprocess_for_humans_library/subprocess_for_humans.py
@@ -3,5 +3,5 @@ import subprocess
 
 def run_script(exec): #Exec muesst ein List sein ob ihr wollen mehr denn eine Befehl
     subprocess.run(args=exec, returncode=0)
-def run_script_with_ouput(exec):
+def run_script_with_ouput(exec): #WARNUNG: These kann ein Sicherheit Problem sein veil ich gemacht die shell=True Argument!
     return subprocess.run(exec, shell=True, check=True, stdout=subprocess.PIPE)
